82d7ff3b3686ccfc6ea1edf3c0eea450dfda345f
dbsgur <kkr431@naver.com>
2022-03-15 18:55:21 +0900

    3/15

---
+++ data structure/implement/linkedList.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// max stack size
+#define SIZE 1000
+#define TRUE 1
+#define FALSE 0
+
+typedef struct __list
+{
+  struct node *cur;
+  struct node *head;
+  struct node *tail;
+
+} linkedList;
+
+typedef struct __node
+{
+  int data;
+  struct __node *next;
+} node;
+
+//create node
+void createNode(linkedList *L, int tdata)
+{
+  node *newNode = (node *)malloc(sizeof(node));
+  newNode->data = tdata;
+  newNode->next = NULL;
+  if (L->head == NULL && L->tail == NULL)
+  {
+    L->head = L->tail = newNode;
+  }
+  else
+  {
+    L->tail->next = newNode;
+    L->tail = newNode;
+  }
+  L->cur = newNode;
+}
+
+// push
+
+// delete
